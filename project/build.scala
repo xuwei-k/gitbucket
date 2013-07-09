069d9b57deea77d4ce1c7d8d1a60ca3d4295c092
xuwei-k <6b656e6a69@gmail.com>
2013-07-09 20:28:07 +0900

    heroku

--- project/build.scala
+++ project/build.scala
@@ -1,7 +1,7 @@
 import sbt._
 import Keys._
 import org.scalatra.sbt._
-import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
+import com.typesafe.sbt.SbtStartScript
 import play.twirl.sbt.SbtTwirl
 import play.twirl.sbt.Import.TwirlKeys._
 
@@ -17,6 +17,7 @@ object MyBuild extends Build {
     file(".")
   )
   .settings(ScalatraPlugin.scalatraWithJRebel: _*)
+  .settings(SbtStartScript.startScriptForClassesSettings: _*)
   .settings(
     sourcesInBase := false,
     organization := Organization,
@@ -32,7 +33,6 @@ object MyBuild extends Build {
       "org.eclipse.jgit" % "org.eclipse.jgit.http.server" % "3.4.1.201406201815-r",
       "org.eclipse.jgit" % "org.eclipse.jgit.archive" % "3.4.1.201406201815-r",
       "org.scalatra" %% "scalatra" % ScalatraVersion,
-      "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
       "org.scalatra" %% "scalatra-json" % ScalatraVersion,
       "org.json4s" %% "json4s-jackson" % "3.2.10",
       "jp.sf.amateras" %% "scalatra-forms" % "0.1.0",
@@ -46,12 +46,11 @@ object MyBuild extends Build {
       "com.novell.ldap" % "jldap" % "2009-10-07",
       "com.h2database" % "h2" % "1.4.180",
       "ch.qos.logback" % "logback-classic" % "1.0.13" % "runtime",
-      "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container;provided",
-      "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts Artifact("javax.servlet", "jar", "jar"),
-      "junit" % "junit" % "4.11" % "test",
+      "org.eclipse.jetty" % "jetty-webapp" % "9.1.3.v20140225" % "container;provided",
+      "org.eclipse.jetty" % "jetty-plus" % "9.1.3.v20140225" % "compile;container",
+      "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts Artifact("javax.servlet", "jar", "jar"),
       "com.typesafe.play" %% "twirl-compiler" % "1.0.2"
     ),
-    EclipseKeys.withSource := true,
     javacOptions in compile ++= Seq("-target", "7", "-source", "7"),
     testOptions in Test += Tests.Argument(TestFrameworks.Specs2, "junitxml", "console"),
     packageOptions += Package.MainClass("JettyLauncher")
